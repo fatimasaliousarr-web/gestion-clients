68fbc0aafb999bb4f56360795677462c711fed25
fatimasaliousarr-web <fatimasaliousarr@gmail.com>
2026-02-03 19:01:27 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,65 @@
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const phoneInput = document.getElementById("phone");
+const addBtn = document.getElementById("addBtn");
+const clientList = document.getElementById("clientList");
+
+let clients = [];
+
+/* Ajouter client */
+addBtn.addEventListener("click", function () {
+
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const phone = phoneInput.value;
+
+  if(name === "" || email === "" || phone === ""){
+    alert("Veuillez remplir tous les champs");
+    return;
+  }
+
+  const client = {
+    name,
+    email,
+    phone
+  };
+
+  clients.push(client);
+  displayClients();
+  clearInputs();
+});
+
+/* Affichage */
+function displayClients(){
+  clientList.innerHTML = "";
+
+  clients.forEach((client, index) => {
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td>${client.name}</td>
+      <td>${client.email}</td>
+      <td>${client.phone}</td>
+      <td>
+        <button class="action-btn" onclick="deleteClient(${index})">Supprimer</button>
+      </td>
+    `;
+
+    clientList.appendChild(row);
+  });
+}
+
+/* Supprimer */
+function deleteClient(index){
+  clients.splice(index,1);
+  displayClients();
+}
+
+/* Reset form */
+function clearInputs(){
+  nameInput.value="";
+  emailInput.value="";
+  phoneInput.value="";
+}
 
